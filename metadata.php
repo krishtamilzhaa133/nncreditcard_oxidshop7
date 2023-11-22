75fb32ee58cc5ddb0f4a190a01650e787a3760cd
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-22 15:29:56 +0530

    Add files via upload

---
+++ metadata.php
@@ -0,0 +1,38 @@
+<?php
+
+$sMetadataVersion = '2.1';
+
+
+$aModule = [
+        'id'          =>'nncreditcard',
+        'title'       => [
+            'de' => 'Novalnet Credit Card Payment',
+            'en' => 'Novalnet Credit Card Payment',
+        ],
+        'description' => [ 'de' => 'This Extension for Novalnet Card Payment',
+                           'en' => 'This Extension for Novalnet Card Payment',
+        ],
+        'thumbnail'   => 'img/nncard.png',
+        'version'     => '2.4.0',
+        'author'      => 'Novalnet Developer',
+        'url'         => 'https://www.novalnet.com',
+        'email'       => 'technical@novalnet.de',
+        'extend'      => [
+         
+         
+        ],
+        'controllers'  => [  
+              
+            
+        ],
+
+
+        'settings'      => [
+         
+            
+            
+        ],
+        'events'    => [
+           
+        ],
+];
---
+++ views/admin_twig/en/nncreditcard_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+$sLangName = 'English';
+
+$aLang = array(
+
+    'charset' => 'UTF-8',
+   
+    
+    //admin lang
+
+);
