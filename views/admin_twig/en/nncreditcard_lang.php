6012ce75fc1f0f00dc497b935b69f9e0dcf760a8
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-28 18:59:10 +0530

    Add files via upload

---
+++ views/admin_twig/en/nncreditcard_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+$sLangName = 'English';
+
+$aLang = array(
+
+    'charset' => 'UTF-8',
+    'SHOP_MODULE_GROUP_nncreditcardpayment' => 'Novalnet Credit Card Payment Configurations',
+    'SHOP_MODULE_nncreditcardtestmode'=>'Credit Card Test Mode',
+    'SHOP_MODULE_nncreditcardpaymentaction'=>'Credit Card Payment Action',
+    'SHOP_MODULE_nncreditcardinline'=>'Credit Card Inline',
+    'SHOP_MODULE_nncreditcard3dsecure'=>'Credit Card 3D Secure',
+
+);
